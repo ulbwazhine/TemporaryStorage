bc910b73e53774cb49a771169a768a079ea9110d
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-23 21:30:18 +0500

    update README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license='MIT',
     author='Ulbwazhine',
     author_email='ulbwa@icloud.com',
-    description='A simple library for temporary storage of small files.',
+    description='An simple library for temporary storage of small files.',
     install_requires=[line.strip() for line in open("requirements.txt").readlines()],
     long_description=open('readme.md', 'r').read(),
     long_description_content_type='text/markdown',
