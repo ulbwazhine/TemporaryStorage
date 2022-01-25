af2cb87f8e83ea193bccda456802ecbf162c553c
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-25 15:03:28 +0500

    2022.01.25.2

--- TemporaryStorage/__main__.py
+++ TemporaryStorage/__main__.py
@@ -3,7 +3,6 @@ from TemporaryStorage import TemporaryStorageInstance
 
 def main():
     storage = TemporaryStorageInstance()
-    print(storage.__create_providers_list__())
     print(storage.upload(input('Enter path to file: ')).url)
 
 
