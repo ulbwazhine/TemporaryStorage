f4cb73c1a996e48273f86039c39658e67996d70f
ульба <81091299+ulbwa@users.noreply.github.com>
2022-01-25 15:10:05 +0500

    2022.01.25.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='TemporaryStorage',
-    version='2022.01.25.2',
+    version='2022.01.25.3',
     packages=find_packages(),
     url='https://github.com/ulbwazhine/TemporaryStorage',
     license='MIT',
     author='Ulbwazhine',
     author_email='ulbwa@icloud.com',
-    description='An simple library for temporary storage of small files.',
+    description='A simple library for temporary storage of small files.',
     install_requires=[line.strip() for line in open("requirements.txt").readlines()],
     long_description=open('readme.md', 'r').read(),
     long_description_content_type='text/markdown',
